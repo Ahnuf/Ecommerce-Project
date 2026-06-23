3715b93d474f0c7921d3c61118b5e4a0ac1f6721
Ahnuf Zafar <ahnufzafar@example.com>
2026-06-23 14:37:01 +0500

    Created Category, Products, ProductImage models in products/models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from base.models import BaseModel
 
-# Create your models here.
+
+class Category(BaseModel):
+    category_name = models.CharField(max_length=100)
+    category_image = models.ImageField(upload = "category")
+
+class Products(BaseModel):
+    product_name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category ,realated_name="products", on_delete=models.CASCADE)
+    price = models.IntegerField((""))
+    product_description = models.TextField()
+
+class ProductImage(BaseModel):
+    product = models.ForeignKey(Prodcut, related_name="products", on_delete=models.CASCADE)
+    image = models.ImageField(upload="product")
